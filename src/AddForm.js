38e5a5573f923527b2b938322941d2378b5c1171
akasraei <a_kasraei@sadad-co.ir>
2020-02-04 17:07:00 +0330

    add todo app

---
+++ src/AddForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class AddForm extends Component {
+
+    state={
+        content:''
+    }
+
+
+    handleChange = (e) => {
+        this.setState({ content: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo({ id: Date.now(), content: this.state.content });
+        this.setState({content:''});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="description"></label>
+                    <input id="description" type="text" onChange={this.handleChange} value={this.state.content} />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddForm;
